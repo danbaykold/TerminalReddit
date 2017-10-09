3c59d64f1861d9b6fe5d6631bf809e0fbf2c8aca
Little Mayo <therealmelon123@gmail.com>
2017-10-08 20:58:51 -0400

    Added basic subreddit browsing

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import praw
+from colorama import Fore, Back, Style
+import argparse
 
-client_id = ""
-client_secret = ""
-password = ""
-username = ""
+client_id = "PoEAOSBSTbnD4Q"
+client_secret = "R-SavcQGJvBMNkSp4xvBUb26VHg"
+password = "dennisisgay234"
+username = "pp420420"
+
 
 reddit = praw.Reddit(
     client_id=client_id,
     client_secret=client_secret,
     password=password,
-    user_agent="Terminal2Reddit v2.0.0",
+    user_agent="TerminalReddit",
     username=username)
 
-subreddit = reddit.subreddit("python")
-hot_python = subreddit.hot(limit = 5)
 
+#subreddit_visit = reddit.subreddit(subreddit)
+#hot_posts = subreddit_visit.hot(limit = 5)
+
+"""
 for submission in hot_python:
     if not submission.stickied:
         print ("Title: {}, ups: {}, downs: {}, Visited:".format(submission.title,
@@ -22,3 +27,34 @@ for submission in hot_python:
                                                                 submission.title,
                                                                 submission.title,
                                                                 ))
+"""
+def init_prompt():
+    print "TerminalReddit v1.0.0"
+    print "type \"help\" for a list of the currently suppored features/commands"
+    input = raw_input("command:")
+    
+    subreddit()
+
+def subreddit():
+    if input == "subreddit":
+        subreddit_name = raw_input("subreddit (do not include \"r/\") -->")
+        subreddit_path = reddit.subreddit(subreddit_name)
+        subreddit = subreddit_path.hot(limit = 50)
+        if (subreddit == "exit"):
+            print "exit"
+        else:
+            try:
+                for submission in subreddit:
+                    if not submission.stickied:
+                        print submission.title
+                        """
+                        print ("Title: {}, ups: {}, downs: {}, Visited:".format(submission.title,
+                                                                                submission.upvote(),
+                                                                                submission.downvote(),
+                                                                                submission.visited(),
+                                                                                ))
+                                                                                """
+            except Exception:
+                print "the entered subreddit was invalid"
+                init_prompt()
+init_prompt()
